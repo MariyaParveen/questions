e4bd2535b63bcbd8935f17663c3fbaa5b1d1d867
MariyaParveen <112948892+MariyaParveen@users.noreply.github.com>
2023-04-12 21:42:07 +0530

    Add files via upload

---
+++ NSEL.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0; i<n; i++){
+		cin>>arr[i];
+	}
+	stack<int>st;
+	vector<int>res;
+	for(int i=0; i<n; i++){
+		while(!st.empty() && arr[i]<st.top()){
+			st.pop();
+		}
+		if(st.empty()){
+			res.push_back(-1);
+		}
+		else{
+			res.push_back(st.top());
+		}
+		st.push(arr[i]);
+	}
+	for(int i=0; i<res.size(); i++){
+		cout<<res[i]<<" ";
+	}
+}
